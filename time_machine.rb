b4043aa632d80c475bbf5b2771a863e15ee30aa6
Tanner Smith <tanner@tssoftware.net>
2012-12-24 21:53:45 -0500

    Add ruby script to go to each commit and use picture.js to take a picture of a file

--- time_machine.rb
+++ time_machine.rb
@@ -3,11 +3,38 @@ require 'trollop'
 opts = Trollop::options do
     opt :dir, "Git Directory", :type => :string
     opt :file, "File", :type => :string
+    opt :save_dir, "Directory where to save images", :type => :string
 end
 
 Trollop::die :dir, "must be given" if !opts[:dir]
 Trollop::die :file, "must be given" if !opts[:file]
+Trollop::die :save_dir, "must be given" if !opts[:save_dir]
 
 Trollop::die :dir, "must exist" unless File.exist?(opts[:dir])
+Trollop::die :save_dir, "must exist" unless File.exist?(opts[:save_dir])
 
-p opts
+file = opts[:file];
+
+inital_dir = Dir.pwd.sub(' ', '\ ');
+
+Dir.chdir(opts[:dir]) do
+    # Record the current branch so we can get back to it in the end
+    inital_branch = %x(git rev-parse --abbrev-ref HEAD).strip
+
+    # Get hashes of all the commits
+    hashes = %x(git log --format=format:%H).split(/\n/);
+
+    hashes.each { |hash|
+        system "git branch time-machine #{hash}"
+        system "git checkout time-machine"
+
+        result = system "phantomjs #{inital_dir}/picture.js #{file} #{opts[:save_dir]}#{hash}.png"
+
+        system "git checkout #{inital_branch}"
+        system "git branch -d time-machine"
+
+        if !result
+            exit(-1);
+        end
+    }
+end

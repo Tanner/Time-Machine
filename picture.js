97fa3c16fa97b4ec9ab6317b3ea70b5c5949d20a
Tanner Smith <tanner@tssoftware.net>
2012-12-24 21:54:00 -0500

    Change the viewport size to a Macbook Pro Retina.

--- picture.js
+++ picture.js
@@ -9,6 +9,8 @@ if (system.args.length === 2) {
 var address = system.args[1];
 var file_name = system.args[2];
 
+page.viewportSize = { width: 2880, height: 1800 };
+
 page.open(address, function(status) {
     page.render(file_name);
 
